d246d4a0b810f28e90a64a18ed29ffa55a7372e8
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-12 19:10:15 +0000

    Square and square root completed, payments has issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_square
-    @num = params.fetch("number").to_i
+    @num = params.fetch("number").to_f
     @square_of_num = @num * @num
     render ({ :template => "calculation_templates/square_results.html.erb"})
   end
@@ -13,8 +13,21 @@ class ApplicationController < ActionController::Base
     render ({ :template => "calculation_templates/squareroot_form.html.erb"})
   end
 
+  def calculate_square_root
+    @num = params.fetch("number").to_f
+    @square_root_of_num = @num ** 0.5
+    render ({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
   def blank_payment_form
     render ({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    @apr = params.fetch("apr").to_i
+    @years = params.fetch("years").to_i
+    @principal = params.fetch("principal").to_i
+    render ({ :template => "calculation_templates/payment_results.html.erb"})
   end
 
   def blank_random_form
