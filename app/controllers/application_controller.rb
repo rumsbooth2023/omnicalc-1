de80f7dfee6dd0c7717f83c8be10de361938fc3f
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-13 15:21:02 +0000

    Corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-    render ({ :template => "calculation_templates/square_form.html.erb"})
+    render({ :template => "calculation_templates/square_form.html.erb"})
   end
 
   def calculate_square
     @num = params.fetch("number").to_f
     @square_of_num = @num * @num
-    render ({ :template => "calculation_templates/square_results.html.erb"})
+    render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
   def blank_square_root_form
-    render ({ :template => "calculation_templates/squareroot_form.html.erb"})
+    render({ :template => "calculation_templates/squareroot_form.html.erb"})
   end
 
   def calculate_square_root
     @num = params.fetch("number").to_f
     @square_root_of_num = @num ** 0.5
-    render ({ :template => "calculation_templates/square_root_results.html.erb"})
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
   end
 
   def blank_payment_form
-    render ({ :template => "calculation_templates/payment_form.html.erb"})
+    render({ :template => "calculation_templates/payment_form.html.erb"})
   end
 
   def calculate_payment
@@ -33,17 +33,17 @@ class ApplicationController < ActionController::Base
     @denominator = 1-(1+@rate_per_period)**-@no_of_periods
     @paymentfull = @numerator / @denominator
     @payment = @paymentfull.to_f.round(2)
-    render ({ :template => "calculation_templates/payment_results.html.erb"})
+    render({ :template => "calculation_templates/payment_results.html.erb"})
   end
 
   def blank_random_form
-    render ({ :template => "calculation_templates/random_form.html.erb"})
+    render({ :template => "calculation_templates/random_form.html.erb"})
   end
 
   def calculate_random
     @min = params.fetch("min").to_i
     @max = params.fetch("max").to_i
     @randomnum = rand(@min..@max).to_f
-    render ({ :template => "calculation_templates/random_results.html.erb"})
+    render({ :template => "calculation_templates/random_results.html.erb"})
   end
 end
